f8dfcad35f40e9ddd8cfabc0552d513f13bc5aeb
mugambi645 <mugambi645@gmail.com>
2021-09-19 13:10:27 +0300

    products selection checkboxes

--- js/index.js
+++ js/index.js
@@ -7,4 +7,9 @@ $(document).ready(function() {
     }
         );
 });
-
+//style the view products button
+$(document).ready(() => {
+    $("#see-button").css({"background-color": "#661A00", 
+    "color": "white", "padding": "0.5rem", "margin-left": "1rem"})
+   
+})
